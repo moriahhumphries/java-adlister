08ba0fc837dbc6e32c680aa2ce3f19ea5b596675
Moriah Humphries <moriah.humphries@gmail.com>
2020-06-02 14:32:23 -0500

    updated message in hello exercise to reflect bonus language for passed name parameter

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -13,7 +13,7 @@ public class HelloWorldServlet extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         String name = request.getParameter("name");
-        out.println("<h1>Hello, " + name + " World</h1>");
+        out.println("<h1>Hello, " + name + "</h1>");
 
     }
 }
